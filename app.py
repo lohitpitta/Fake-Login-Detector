e817963d22cd8f84d24a9b5db43ae649a8a2d9c8
LohitPitta <lohitpitta48@gmail.com>
2026-07-08 21:34:47 +0530

    Modify app.run to use dynamic port from environment
    Update app to use environment variable for port.

--- app.py
+++ app.py
@@ -39,5 +39,8 @@ def index():
     
     return render_template('index.html', result=result, status=status)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
